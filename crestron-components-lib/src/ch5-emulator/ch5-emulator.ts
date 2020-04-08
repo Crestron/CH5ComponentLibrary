f3c887aee8333a115c4298780513ac7f27c902e0
Suresh Kumar M R <mr.sureshkumar@gmail.com>
2020-04-08 12:29:07 +0530

    Putting back the settimeout
    Settimeout function added.

--- crestron-components-lib/src/ch5-emulator/ch5-emulator.ts
+++ crestron-components-lib/src/ch5-emulator/ch5-emulator.ts
@@ -211,7 +211,7 @@ export class Ch5Emulator {
     }
 
     public processAction(action:IEmulatorAction, actionIndex:number, actionArray:IEmulatorAction[], cueSignalValue:boolean|number|string|object){
-        this.processActionAsync(action, actionIndex, actionArray, cueSignalValue);
+        setTimeout(() => this.processActionAsync(action, actionIndex, actionArray, cueSignalValue));
     }
 
     private processActionAsync(action:IEmulatorAction, actionIndex:number, actionArray:IEmulatorAction[], cueSignalValue:boolean|number|string|object){
