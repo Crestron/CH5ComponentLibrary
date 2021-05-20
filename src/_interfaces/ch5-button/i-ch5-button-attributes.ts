45b44a53d2a6c023552a698cf21facb43aa7c5a2
Raghavendra Kumar Dabbir <rdabbir@crestron.com>
2021-05-20 19:14:51 +0530

    CH5-Button fixes

--- src/_interfaces/ch5-button/i-ch5-button-attributes.ts
+++ src/_interfaces/ch5-button/i-ch5-button-attributes.ts
@@ -17,7 +17,61 @@ import {
 } from "./types";
 
 /**
- * @ignore
+ * @name Ch5 Button
+ * @isattribute false
+ * @tagName ch5-button
+ * @role button
+ * @description Ch5 Button offers a wide range of functionality out-of-the-box.
+ * @documentation
+ * [
+ *   "`ch5-button` element",
+ *   "***",
+ *   "An extension to standard HTML ch5-button element with icons and simple formats."
+ * ]
+ * @snippets
+ * [
+ *  {
+ *    "prefix": "ch5-button:blank",
+ *     "description": "Crestron Button",
+ *     "body": [
+ *       "<ch5-button>",
+ *       "</ch5-button>$0"
+ *     ]
+ *   },
+ *   {
+ *    "prefix": "ch5-button:default",
+ *     "description": "Crestron Button",
+ *     "body": [
+ *       "<ch5-button id=\"btn_${1:id}\"",
+ *       "\tlabel=\"${2:Crestron Button}\"",
+ *       "\tsendeventonclick=\"${3:btn_${1}_clicked}\">",
+ *       "</ch5-button>$0"
+ *     ]
+ *   },
+ *   {
+ *     "prefix": "ch5-button:all-attributes",
+ *     "description": "Crestron Button (All Attributes)",
+ *     "body": [
+ *       "<ch5-button id=\"btn_${1:id}\"",
+ *       "\tlabel=\"${2:Crestron Button}\"",
+ *       "\ttype=\"${3|default,primary,info,text,danger,warning,success,secondary|}\"",
+ *       "\tcustomClass=\"${4:customClass}\"",
+ *       "\tshape=\"${5|rounded-rectangle,rectangle,tab,circle,oval|}\"",
+ *       "\tsize=\"${6|regular,x-small,small,large,x-large|}\"",
+ *       "\tstretch=\"${7|both,width,height|}\"",
+ *       "\ticonposition=\"${8|first,last,top,bottom|}\"",
+ *       "\ticonclass=\"${9:iconClass}\"",
+ *       "\torientation=\"${10|horizontal,vertical|}\"",
+ *       "\tsendeventonclick=\"${11:btn_${1}_clicked}\"",
+ *       "\tsendeventontouch=\"${12:btn_${1}_touched}\"",
+ *       "\treceivestateselected=\"${13}\"",
+ *       "\treceivestatelabel=\"${14}\"",
+ *       "\treceivestatescriptlabelhtml=\"${15}\">",
+ *       "</ch5-button>$0"
+ *     ]
+ *   }
+ * ]
+ *
  */
 export interface ICh5ButtonAttributes extends ICh5CommonAttributes {
     /**
@@ -79,19 +133,19 @@ export interface ICh5ButtonAttributes extends ICh5CommonAttributes {
      */
     iconUrl: string;
 
-      /**
-   * @documentation
-   * [
-   * "`checkboxShow` attribute",
-   * "***",
-   * "The default value is false.",
-   * "This property is used to display or hide a checkbox. If set to true, a checkbox is displayed and ",
-   * "'ch5-button__checkbox' will be applied as the CSS class on the component."
-   * ]
-   * @name checkboxShow
-   * @default false
-   */
-   checkboxShow: boolean;
+    /**
+ * @documentation
+ * [
+ * "`checkboxShow` attribute",
+ * "***",
+ * "The default value is false.",
+ * "This property is used to display or hide a checkbox. If set to true, a checkbox is displayed and ",
+ * "'ch5-button__checkbox' will be applied as the CSS class on the component."
+ * ]
+ * @name checkboxShow
+ * @default false
+ */
+    checkboxShow: boolean;
 
     /**
      * @documentation
@@ -296,4 +350,17 @@ export interface ICh5ButtonAttributes extends ICh5CommonAttributes {
      */
     customClassDisabled: string | null;
 
+    /**
+      * @documentation
+      * [
+      * "`selected` attribute",
+      * "***",
+      * "The default value is false.",
+      * "This property reflects the state of the component. If set to true, ",
+      * "'ch5-button--selected' will be applied as the CSS class on the component."
+      * ]
+      * @name selected
+      * @default false
+      */
+    selected: boolean;
 }
--- src/_interfaces/ch5-button/i-ch5-button.ts
+++ src/_interfaces/ch5-button/i-ch5-button.ts
@@ -7,64 +7,9 @@
 
 import { ICh5ButtonAttributes } from "./i-ch5-button-attributes";
 import { ICh5Common } from "../ch5-common";
-import { TBoolAttribute } from "../ch5-common/types/t-bool-attribute";
 
 /**
- * @name Ch5 Button
- * @isattribute false
- * @tagName ch5-button
- * @role button
- * @description Ch5 Button offers a wide range of functionality out-of-the-box.
- * @documentation
- * [
- *   "`ch5-button` element",
- *   "***",
- *   "An extension to standard HTML ch5-button element with icons and simple formats."
- * ]
- * @snippets
- * [
- *  {
- *    "prefix": "ch5-button:blank",
- *     "description": "Crestron Button",
- *     "body": [
- *       "<ch5-button>",
- *       "</ch5-button>$0"
- *     ]
- *   },
- *   {
- *    "prefix": "ch5-button:default",
- *     "description": "Crestron Button",
- *     "body": [
- *       "<ch5-button id=\"btn_${1:id}\"",
- *       "\tlabel=\"${2:Crestron Button}\"",
- *       "\tsendeventonclick=\"${3:btn_${1}_clicked}\">",
- *       "</ch5-button>$0"
- *     ]
- *   },
- *   {
- *     "prefix": "ch5-button:all-attributes",
- *     "description": "Crestron Button (All Attributes)",
- *     "body": [
- *       "<ch5-button id=\"btn_${1:id}\"",
- *       "\tlabel=\"${2:Crestron Button}\"",
- *       "\ttype=\"${3|default,primary,info,text,danger,warning,success,secondary|}\"",
- *       "\tcustomClass=\"${4:customClass}\"",
- *       "\tshape=\"${5|rounded-rectangle,rectangle,tab,circle,oval|}\"",
- *       "\tsize=\"${6|regular,x-small,small,large,x-large|}\"",
- *       "\tstretch=\"${7|both,width,height|}\"",
- *       "\ticonposition=\"${8|first,last,top,bottom|}\"",
- *       "\ticonclass=\"${9:iconClass}\"",
- *       "\torientation=\"${10|horizontal,vertical|}\"",
- *       "\tsendeventonclick=\"${11:btn_${1}_clicked}\"",
- *       "\tsendeventontouch=\"${12:btn_${1}_touched}\"",
- *       "\treceivestateselected=\"${13}\"",
- *       "\treceivestatelabel=\"${14}\"",
- *       "\treceivestatescriptlabelhtml=\"${15}\">",
- *       "</ch5-button>$0"
- *     ]
- *   }
- * ]
- *
+ * @ignore
  */
 export interface ICh5Button extends ICh5ButtonAttributes, ICh5Common {
 
@@ -89,20 +34,6 @@ export interface ICh5Button extends ICh5ButtonAttributes, ICh5Common {
    * @name onrelease
    */
   onrelease: string;
-
-  /**
-   * @documentation
-   * [
-   * "`selected` attribute",
-   * "***",
-   * "The default value is false.",
-   * "This property reflects the state of the component. If set to true, ",
-   * "'ch5-button--selected' will be applied as the CSS class on the component."
-   * ]
-   * @name selected
-   * @default false
-   */
-  selected: TBoolAttribute;
 
   /**
    * @documentation
