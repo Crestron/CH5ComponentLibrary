b22f2cf968692f1c696100876b55d84fec3af1c2
himadharh <72376060+himadharh@users.noreply.github.com>
2021-05-27 17:31:51 +0530

    i-ch5-overlay-panel related documentation changes to obsolete i-ch5-overlay-panel.ts
    i-ch5-overlay-panel related documentation changes to obsolete i-ch5-overlay-panel.ts

--- src/ch5-overlay-panel/interfaces/i-ch5-overlay-panel-attributes.ts
+++ src/ch5-overlay-panel/interfaces/i-ch5-overlay-panel-attributes.ts
@@ -9,6 +9,63 @@ import { TCh5OverlayPanelStretch, TCh5OverlayPanelOverflow, TCh5OverlayPanelPosi
 import { ICh5CommonAttributes } from "../../ch5-common/interfaces";
 
 /**
+ * @name Ch5 Overlay Panel
+ * @isattribute false
+ * @tagName ch5-overlay-panel
+ * @role dialog
+ * @description Ch5 Overlay Panel
+ * @componentVersion 1.0.0
+ * @documentation
+ * [
+ * "`ch5-overlay-panel` element",
+ * "***",
+ * "The overlay panel component provides a content container for other components that ",
+ * "'pop up' on top of and overlays the main content container."
+ * ]
+ * @snippets
+ * [
+ *    {
+ *       "prefix": "ch5-overlay-panel:blank",
+ *       "description": "Crestron Overlay Panel (Blank)",
+ *       "body": [
+ *           "<ch5-overlay-panel>",
+ *           "</ch5-overlay-panel>$0"
+ *       ]
+ *    },
+ *    {
+ *       "prefix": "ch5-overlay-panel:default",
+ *       "description": "Crestron Overlay Panel (Default)",
+ *       "body": [
+ *           "<ch5-overlay-panel receivestateshowpulse=\"${1:trigger_1}\" closable>",
+ *           "    <p>Sample text</p>",
+ *          "    <ch5-image id=\"${2:ex1-img}\" url=\"${3:some_server_url}\">",
+ *           "    </ch5-image>",
+ *           "</ch5-overlay-panel>$0"
+ *       ]
+ *    },
+ *    {
+ *        "prefix": "ch5-overlay-panel:signals",
+ *        "description": "Crestron Overlay Panel Signals",
+ *        "body": [
+ *            "<ch5-overlay-panel",
+ *            "    receivestateshowpulse=\"${1:trigger_1}\"",
+ *            "    closable",
+ *           "    sendeventonshow=\"${1:signal_on_show}\"",
+ *            "    sendeventonbeforeshow=\"${2:signal_on_before_show}\"",
+ *            "    sendeventonaftershow=\"${3:signal_on_after_show}\"",
+ *            "    sendeventonbeforehide=\"${4:signal_on_before_hide}\"",
+ *            "    sendeventonafterhide=\"${5:signal_on_after_hide}\"",
+ *            "    >",
+ *            "    <p>Sample text</p>",
+ *            "    <ch5-image id=\"${8:ex1-img}\" url=\"${9:some_server_url}\">",
+ *            "    </ch5-image>",
+ *            "</ch5-overlay-panel>$0"
+ *        ]
+ *    }
+ * ]
+ */
+
+/**
  * @ignore
  */
 export interface ICh5OverlayPanelAttributes extends ICh5CommonAttributes {
@@ -76,6 +133,40 @@ export interface ICh5OverlayPanelAttributes extends ICh5CommonAttributes {
    * @name positionoffset
    */
   positionOffset: TCh5OverlayPanelPositionOffset
+
+  /**
+   * @documentation
+   * [
+   * "`dismissable` attribute",
+   * "***",
+   * "The default value is 'true'. If false, the panel won't automatically hide if a touch event occurs outside of it."
+   * ]
+   * @name dismissable
+   */
+  dismissable: boolean;
+  
+  /**
+   * @documentation
+   * [
+   * "`closable` attribute",
+   * "***",
+   * "The default value is 'false'. If true, shows a close icon on the corner of the panel. If false, ",
+   * "the close icon is not shown."
+   * ]
+   * @name closable
+   */
+  closable: boolean;
+
+  /**
+   * @documentation
+   * [
+   * "`mask` attribute",
+   * "***",
+   * "Specifies whether or not to apply a background mask."
+   * ]
+   * @name mask
+   */
+  mask: boolean;
 
   /**
    * @documentation
