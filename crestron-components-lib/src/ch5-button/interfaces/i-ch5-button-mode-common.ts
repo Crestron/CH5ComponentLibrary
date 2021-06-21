56cd689ba1357b3711358ac1b3ebd8d2313c51cf
Raghavendra Kumar Dabbir <rdabbir@crestron.com>
2021-06-21 09:59:08 +0530

    Ch5 button fixes
    Button fixes for mode and introducing ch5-log

--- crestron-components-lib/src/ch5-button/interfaces/i-ch5-button-mode-common.ts
+++ crestron-components-lib/src/ch5-button/interfaces/i-ch5-button-mode-common.ts
@@ -66,96 +66,99 @@ import { ICh5CommonAttributes } from "../../ch5-common/interfaces/";
  * ]
  *
  */
-export interface ICh5ButtonModeCommonAttributes extends ICh5CommonAttributes {
+export interface ICh5ButtonModeCommonAttributes {
 
-    /**
-     * @documentation
-     * [
-     * "`type` attribute",
-     * "***",
-     * "Valid values: 'default', 'info', 'text', 'danger', 'warning', 'success', 'primary', 'secondary'.",
-     * "Overrides the appearance of the button with alternative CSS ",
-     * "defined in classes defined with ch5-button--type, where type is ",
-     * "the value of the property. If no 'type' is provided, the type of ",
-     * "'default' is used."
-     * ]
-     * @name type
-     */
-    type: TCh5ButtonType | null;
+  /**
+   * @documentation
+   * [
+   * "`type` attribute",
+   * "***",
+   * "Valid values: 'default', 'info', 'text', 'danger', 'warning', 'success', 'primary', 'secondary'.",
+   * "Overrides the appearance of the button with alternative CSS ",
+   * "defined in classes defined with ch5-button--type, where type is ",
+   * "the value of the property. If no 'type' is provided, the type of ",
+   * "'default' is used."
+   * ]
+   * @name type
+   */
+  type: TCh5ButtonType | null;
 
-    /**
-     * @documentation
-     * [
-     * "`icon` attribute",
-     * "***",
-     * "The icon class."
-     * ]
-     * @name iconclass
-     */
-    iconClass: string;
+  /**
+   * @documentation
+   * [
+   * "`icon` attribute",
+   * "***",
+   * "The icon class."
+   * ]
+   * @name iconclass
+   */
+  iconClass: string;
 
-    /**
-     * @documentation
-     * [
-     * "`iconposition` attribute",
-     * "***",
-     * "The default value is 'first'.",
-     * "Valid values: 'first', 'last', 'top', 'bottom'.",
-     * "The icon position relative to the label."
-     * ]
-     * @name iconposition
-     * @default first
-     */
-    iconPosition: TCh5ButtonIconPosition | null;
+  /**
+   * @documentation
+   * [
+   * "`iconposition` attribute",
+   * "***",
+   * "The default value is 'first'.",
+   * "Valid values: 'first', 'last', 'top', 'bottom'.",
+   * "The icon position relative to the label."
+   * ]
+   * @name iconposition
+   * @default first
+   */
+  iconPosition: TCh5ButtonIconPosition | null;
 
-    /**
-     * @documentation
-     * [
-     * "`iconurl` attribute",
-     * "***",
-     * "The attribute used for add a SVG image."
-     * ]
-     * @name iconurl
-     */
-    iconUrl: string;
+  /**
+   * @documentation
+   * [
+   * "`iconurl` attribute",
+   * "***",
+   * "The attribute used for add a SVG image."
+   * ]
+   * @name iconurl
+   */
+  iconUrl: string;
 
-    /**
-     * @documentation
-     * [
-     * "`checkboxPosition` attribute",
-     * "***",
-     * "The default value is false.",
-     * "This property is used to display or hide a checkbox. If set to true, a checkbox is displayed and ",
-     * "'ch5-button__checkbox' will be applied as the CSS class on the component."
-     * ]
-     * @name checkboxPosition
-     * @default false
-     */
-    checkboxPosition: TCh5ButtonCheckboxPosition | null;
+  /**
+   * @documentation
+   * [
+   * "`customClass` attribute",
+   * "***",
+   * "The attribute used for add a SVG image."
+   * ]
+   * @name customClass
+   */
+  customClass: string;
 
-    /**
-      * @documentation
-      * [
-      * "`hAlignLabel` attribute",
-      * "***",
-      * "Valid values: 'width', 'height', 'both'.",
-      * "When the stretch property is set, the button element inherits the ",
-      * "width and/or height of the container. If stretch=height is used, ",
-      * "the button will be responsive based on the label length until it ",
-      * "reaches the max-width of the container. If stretch=width is ",
-      * "applied, there is no responsiveness after reaching the max-width, and ",
-      * "the text will overflow. The same applies if stretch=both is used. Note ",
-      * "that if the button element shape is 'circle' or 'oval', the stretch ",
-      * "property will be ignored."
-      * ]
-      * @name hAlignLabel
-      */
-    hAlignLabel: TCh5ButtonHorizontalAlignLabel | null;
+  /**
+   * @documentation
+   * [
+   * "`customStyle` attribute",
+   * "***",
+   * "The attribute used for add a SVG image."
+   * ]
+   * @name customStyle
+   */
+  customStyle: string;
 
-    /**
+  /**
+   * @documentation
+   * [
+   * "`checkboxPosition` attribute",
+   * "***",
+   * "The default value is false.",
+   * "This property is used to display or hide a checkbox. If set to true, a checkbox is displayed and ",
+   * "'ch5-button__checkbox' will be applied as the CSS class on the component."
+   * ]
+   * @name checkboxPosition
+   * @default false
+   */
+  checkboxPosition: TCh5ButtonCheckboxPosition | null;
+
+  /**
     * @documentation
     * [
-    * "`vAlignLabel` attribute",
+    * "`hAlignLabel` attribute",
     * "***",
     * "Valid values: 'width', 'height', 'both'.",
     * "When the stretch property is set, the button element inherits the ",
@@ -167,7 +170,26 @@ export interface ICh5ButtonModeCommonAttributes extends ICh5CommonAttributes {
     * "that if the button element shape is 'circle' or 'oval', the stretch ",
     * "property will be ignored."
     * ]
-    * @name vAlignLabel
+    * @name hAlignLabel
     */
-    vAlignLabel: TCh5ButtonVerticalAlignLabel | null;
+  hAlignLabel: TCh5ButtonHorizontalAlignLabel | null;
+
+  /**
+  * @documentation
+  * [
+  * "`vAlignLabel` attribute",
+  * "***",
+  * "Valid values: 'width', 'height', 'both'.",
+  * "When the stretch property is set, the button element inherits the ",
+  * "width and/or height of the container. If stretch=height is used, ",
+  * "the button will be responsive based on the label length until it ",
+  * "reaches the max-width of the container. If stretch=width is ",
+  * "applied, there is no responsiveness after reaching the max-width, and ",
+  * "the text will overflow. The same applies if stretch=both is used. Note ",
+  * "that if the button element shape is 'circle' or 'oval', the stretch ",
+  * "property will be ignored."
+  * ]
+  * @name vAlignLabel
+  */
+  vAlignLabel: TCh5ButtonVerticalAlignLabel | null;
 }
