c07773caa02dd9ae35e4ca5149268e235949e89d
Raghavendra Kumar Dabbir <rdabbir@crestron.com>
2021-06-23 17:40:27 +0530

    Documentation fixes

--- src/ch5-button/interfaces/i-ch5-button-documentation.ts
+++ src/ch5-button/interfaces/i-ch5-button-documentation.ts
@@ -9,8 +9,96 @@ import { ICh5ButtonAttributes } from ".";
 import { ICh5Common } from "../../ch5-common/interfaces";
 
 /**
- * @ignore
+ * @name Ch5 Button
+ * @isattribute false
+ * @tagName ch5-button
+ * @role button
+ * @description Ch5 Button offers a wide range of functionality out-of-the-box.
+ * @componentVersion 1.0.0
+ * @documentation
+ * [
+ *   "`ch5-button` element",
+ *   "***",
+ *   "A custom component designed to provide options to add icons, label, text, multiselect among other powerful options."
+ * ]
+ * @snippets
+ * [
+ *  {
+ *    "prefix": "ch5-button:blank",
+ *     "description": "Crestron Button",
+ *     "body": [
+ *       "<ch5-button>",
+ *       "</ch5-button>$0"
+ *     ]
+ *   },
+ *   {
+ *    "prefix": "ch5-button:default",
+ *     "description": "Crestron Button",
+ *     "body": [
+ *       "<ch5-button id=\"btn_${1:id}\"",
+ *       "\tlabel=\"${2:Crestron Button}\"",
+ *       "\tsendeventonclick=\"${3:btn_${1}_clicked}\">",
+ *       "</ch5-button>$0"
+ *     ]
+ *   },
+ *   {
+ *     "prefix": "ch5-button:all-attributes",
+ *     "description": "Crestron Button (All Attributes)",
+ *     "body": [
+ *       "<ch5-button id=\"btn_${1:id}\"",
+ *       "\tlabel=\"${2:Crestron Button}\"",
+ *       "\ttype=\"${3|default,primary,info,text,danger,warning,success,secondary|}\"",
+ *       "\tcustomClass=\"${4:customClass}\"",
+ *       "\tshape=\"${5|rounded-rectangle,rectangle,tab,circle,oval|}\"",
+ *       "\tsize=\"${6|regular,x-small,small,large,x-large|}\"",
+ *       "\tstretch=\"${7|both,width,height|}\"",
+ *       "\ticonposition=\"${8|first,last,top,bottom|}\"",
+ *       "\ticonclass=\"${9:iconClass}\"",
+ *       "\torientation=\"${10|horizontal,vertical|}\"",
+ *       "\tsendeventonclick=\"${11:btn_${1}_clicked}\"",
+ *       "\tsendeventontouch=\"${12:btn_${1}_touched}\"",
+ *       "\treceivestateselected=\"${13}\"",
+ *       "\treceivestatelabel=\"${14}\"",
+ *       "\treceivestatescriptlabelhtml=\"${15}\">",
+ *       "</ch5-button>$0"
+ *     ]
+ *   }
+ * ]
+ *
  */
 export interface ICh5ButtonDocumentation extends ICh5Common, ICh5ButtonAttributes {
+
+  /**
+   * @documentation
+   * [
+   * "`onpress` attribute",
+   * "***",
+   * "Runs when a press event is initiated."
+   * ]
+   * @name onpress
+   */
+   onpress: string;
+
+   /**
+    * @documentation
+    * [
+    * "`onrelease` attribute",
+    * "***",
+    * "Runs when a release event is initiated."
+    * ]
+    * @name onrelease
+    */
+   onrelease: string;
+ 
+   /**
+    * @documentation
+    * [
+    * "`customclassselected` attribute",
+    * "***",
+    * "Specifies a custom class for the selected state of the button."
+    * ]
+    * @name customclassselected
+    */
+   customClassSelected: string | null;
 
 }
