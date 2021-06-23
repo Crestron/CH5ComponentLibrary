cf7690d07d3e35d521884c57248691fb463d1ef1
Raghavendra Kumar Dabbir <rdabbir@crestron.com>
2021-06-23 17:40:27 +0530

    Documentation fixes

--- crestron-components-lib/src/ch5-button/interfaces/i-ch5-button-attributes.ts
+++ crestron-components-lib/src/ch5-button/interfaces/i-ch5-button-attributes.ts
@@ -20,62 +20,7 @@ import {
 } from "./t-ch5-button";
 
 /**
- * @name Ch5 Button
- * @isattribute false
- * @tagName ch5-button
- * @role button
- * @description Ch5 Button offers a wide range of functionality out-of-the-box.
- * @componentVersion 1.0.0
- * @documentation
- * [
- *   "`ch5-button` element",
- *   "***",
- *   "A custom component designed to provide options to add icons, label, text, multiselect among other powerful options."
- * ]
- * @snippets
- * [
- *  {
- *    "prefix": "ch5-button:blank",
- *     "description": "Crestron Button",
- *     "body": [
- *       "<ch5-button>",
- *       "</ch5-button>$0"
- *     ]
- *   },
- *   {
- *    "prefix": "ch5-button:default",
- *     "description": "Crestron Button",
- *     "body": [
- *       "<ch5-button id=\"btn_${1:id}\"",
- *       "\tlabel=\"${2:Crestron Button}\"",
- *       "\tsendeventonclick=\"${3:btn_${1}_clicked}\">",
- *       "</ch5-button>$0"
- *     ]
- *   },
- *   {
- *     "prefix": "ch5-button:all-attributes",
- *     "description": "Crestron Button (All Attributes)",
- *     "body": [
- *       "<ch5-button id=\"btn_${1:id}\"",
- *       "\tlabel=\"${2:Crestron Button}\"",
- *       "\ttype=\"${3|default,primary,info,text,danger,warning,success,secondary|}\"",
- *       "\tcustomClass=\"${4:customClass}\"",
- *       "\tshape=\"${5|rounded-rectangle,rectangle,tab,circle,oval|}\"",
- *       "\tsize=\"${6|regular,x-small,small,large,x-large|}\"",
- *       "\tstretch=\"${7|both,width,height|}\"",
- *       "\ticonposition=\"${8|first,last,top,bottom|}\"",
- *       "\ticonclass=\"${9:iconClass}\"",
- *       "\torientation=\"${10|horizontal,vertical|}\"",
- *       "\tsendeventonclick=\"${11:btn_${1}_clicked}\"",
- *       "\tsendeventontouch=\"${12:btn_${1}_touched}\"",
- *       "\treceivestateselected=\"${13}\"",
- *       "\treceivestatelabel=\"${14}\"",
- *       "\treceivestatescriptlabelhtml=\"${15}\">",
- *       "</ch5-button>$0"
- *     ]
- *   }
- * ]
- *
+ * @ignore
  */
 export interface ICh5ButtonAttributes extends ICh5CommonAttributes {
   /**
