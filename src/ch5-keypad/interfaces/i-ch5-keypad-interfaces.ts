a7e2aeda66defad7f5f83e8a89bb1ada51706680
himadharh <72376060+himadharh@users.noreply.github.com>
2021-08-19 18:35:43 +0530

    Keypad: initial commit
    Keypad: initial commit

---
+++ src/ch5-keypad/interfaces/i-ch5-keypad-interfaces.ts
@@ -0,0 +1,12 @@
+// Copyright (C) 2021 to the present, Crestron Electronics, Inc.
+// All rights reserved.
+// No part of this software may be reproduced in any form, machine
+// or natural, without the express written consent of Crestron Electronics.
+// Use of this source code is subject to the terms of the Crestron Software License Agreement
+// under which you licensed this source code.
+
+import { ICh5CommonAttributes } from "../../ch5-common/interfaces";
+
+export interface ICh5KeypadAttributes extends ICh5CommonAttributes {
+
+}
