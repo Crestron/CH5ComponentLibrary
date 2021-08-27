100785c24a2be1fed3d79aa40a10437879c9e919
himadharh <72376060+himadharh@users.noreply.github.com>
2021-08-27 17:36:02 +0530

    Keypad: updated few attributes and documentation related corrections
    Keypad: updated few attributes and documentation related corrections

--- crestron-components-lib/src/ch5-keypad/ch5-keypad-btn-data.ts
+++ crestron-components-lib/src/ch5-keypad/ch5-keypad-btn-data.ts
@@ -1,3 +1,10 @@
+// Copyright (C) 2021 to the present, Crestron Electronics, Inc.
+// All rights reserved.
+// No part of this software may be reproduced in any form, machine
+// or natural, without the express written consent of Crestron Electronics.
+// Use of this source code is subject to the terms of the Crestron Software License Agreement
+// under which you licensed this source code.
+
 import { TCh5KeypadBtnCreateDTO } from "./interfaces/t-ch5-keypad";
 
 export class CH5KeypadBtnData {
