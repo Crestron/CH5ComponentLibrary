e9fbd778a6e1d68fc32ec699758c21ca095ebe9c
Pascalau Loredana <92975236+v-lpascalau-crestron@users.noreply.github.com>
2022-02-09 11:23:07 +0200

    chore: CH5C-2248

--- src/ch5-dpad/ch5-dpad-contract-utils.ts
+++ src/ch5-dpad/ch5-dpad-contract-utils.ts
@@ -10,16 +10,16 @@ export class CH5DpadContractUtils {
     // the join number is applied to the top button, join+1 applies to bottom,
     // join+2 applies to left, join+3 applies to right, join+4 to center
     public static readonly sendEventOnClickSigCountToAdd = {
-        top: 0,
-        bottom: 1,
+        up: 0,
+        down: 1,
         left: 2,
         right: 3,
         center: 4
     };
 
     public static readonly contractSuffix = {
-        top: 'Up',
-        bottom: 'Down',
+        up: 'Up',
+        down: 'Down',
         left: 'Left',
         right: 'Right',
         center: 'Center'
