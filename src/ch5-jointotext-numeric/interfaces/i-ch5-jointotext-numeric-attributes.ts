12d07d18f9ff791dc9209d966ae55ebaeb303349
Raghavendra Kumar Dabbir <rdabbir@crestron.com>
2022-02-24 11:17:50 +0530

    fix: CH5C-2345 for Numeric only
    https://crestroneng.atlassian.net/browse/CH5C-2345 for JoinInfoTextNumeric Only

--- src/ch5-jointotext-numeric/interfaces/i-ch5-jointotext-numeric-attributes.ts
+++ src/ch5-jointotext-numeric/interfaces/i-ch5-jointotext-numeric-attributes.ts
@@ -1,8 +1,11 @@
 import { ICh5CommonAttributes } from "../../ch5-common/interfaces";
 import { NumericFormats } from "../format/numeric-formats";
 
+/**
+ * @ignore
+ */
 export interface ICh5JoinToTextNumericAttributes extends ICh5CommonAttributes {
-   
+
   /**
    * @documentation
    * [
@@ -14,83 +17,83 @@ export interface ICh5JoinToTextNumericAttributes extends ICh5CommonAttributes {
    * @default ""
    * @attributeType "string"
    */
-     value: string;
+  value: string;
 
-    /**
-     * @documentation
-     * [
-     * "`type` attribute",
-     * "***",
-     * "Defines the numeric type."
-     * ]
-     * @name type
-     * @default signed
-     * @attributeType "string"
-     */
-    type: NumericFormats;
+  /**
+   * @documentation
+   * [
+   * "`type` attribute",
+   * "***",
+   * "Defines the numeric type."
+   * ]
+   * @name type
+   * @default signed
+   * @attributeType "string"
+   */
+  type: NumericFormats;
 
-    /**
-     * @documentation
-     * [
-     * "`receiveStateValue` attribute",
-     * "***",
-     * "Defines the receivestatevalue state name."
-     * ]
-     * @name receiveStateValue
-     * @default ""
-     * @attributeType "string"
-     */
-    receiveStateValue: string;
+  /**
+   * @documentation
+   * [
+   * "`receiveStateValue` attribute",
+   * "***",
+   * "Defines the receivestatevalue state name."
+   * ]
+   * @name receiveStateValue
+   * @default ""
+   * @attributeType "string"
+   */
+  receiveStateValue: string;
 
-    /**
-     * @documentation
-     * [
-     * "`decimalLength` attribute",
-     * "***",
-     * "Defines the floating point position."
-     * ]
-     * @name decimalLength
-     * @default 2
-     * @attributeType "string"
-     */
-    decimalLength: number;
+  /**
+   * @documentation
+   * [
+   * "`decimalLength` attribute",
+   * "***",
+   * "Defines the floating point position."
+   * ]
+   * @name decimalLength
+   * @default 2
+   * @attributeType "string"
+   */
+  decimalLength: number;
 
-    /**
-     * @documentation
-     * [
-     * "`length` attribute",
-     * "***",
-     * "Defines the whole number min length."
-     * ]
-     * @name length
-     * @default 0
-     * @attributeType "string"
-     */
-    length: number;
+  /**
+   * @documentation
+   * [
+   * "`length` attribute",
+   * "***",
+   * "Defines the whole number min length."
+   * ]
+   * @name length
+   * @default 0
+   * @attributeType "string"
+   */
+  length: number;
 
-    /**
-     * @documentation
-     * [
-     * "`min` attribute",
-     * "***",
-     * "Defines the min range used only for percentage type."
-     * ]
-     * @name min
-     * @default 0
-     * @attributeType "string"
-     */
-    min: number;
+  /**
+   * @documentation
+   * [
+   * "`min` attribute",
+   * "***",
+   * "Defines the min range used only for percentage type."
+   * ]
+   * @name min
+   * @default 0
+   * @attributeType "string"
+   */
+  min: number;
 
-    /**
-     * @documentation
-     * [
-     * "`max` attribute",
-     * "***",
-     * "Defines the max range used only for percentage type."
-     * ]
-     * @name max
-     * @default 65535
-     * @attributeType "string"
-     */
-    max: number;
+  /**
+   * @documentation
+   * [
+   * "`max` attribute",
+   * "***",
+   * "Defines the max range used only for percentage type."
+   * ]
+   * @name max
+   * @default 65535
+   * @attributeType "string"
+   */
+  max: number;
 }
