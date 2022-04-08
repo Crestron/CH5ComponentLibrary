9429bcd0789191b54e3266168bcf580fd6d126bf
Raghavendra Kumar Dabbir <rdabbir@crestron.com>
2022-04-08 18:32:58 +0530

    fix: CH5C-2336
    fix: CH5C-2336

--- crestron-components-lib/src/ch5-jointotext-numeric/format/signed-format.ts
+++ crestron-components-lib/src/ch5-jointotext-numeric/format/signed-format.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { NumericFormat } from "./numeric-format";
 
 export type SignedFormatOptions = {
@@ -10,6 +11,7 @@ export class SignedFormat extends NumericFormat {
 
         const { length } = options;
 
+        const sign = (!_.isNil(value) && String(value) !== "" && !isNaN(value)) ? ((value < 0) ? "-" : "") : "";
         const MAX_ANALOG = 65535;
         const HALF_MAX_ANALOG = 32767;
 
@@ -25,14 +27,14 @@ export class SignedFormat extends NumericFormat {
         // signed integer - e.g. "%10d" -- adds leading zeros
         const leadingZeros = length - textLength;
         if (leadingZeros > 0 && textLength > 0) {
-            for (let i = 0; i < leadingZeros; i++) { 
-                formattedText += "0"; 
+            for (let i = 0; i < leadingZeros; i++) {
+                formattedText += "0";
             }
         }
 
         formattedText += Math.abs(value);
 
-        return formattedText;
+        return sign + formattedText;
     }
 
 }
