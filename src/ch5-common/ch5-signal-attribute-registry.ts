df0891c39442054624eb349ca84719f4bb676617
Raghavendra Kumar Dabbir <rdabbir@crestron.com>
2022-09-12 15:07:10 +0530

    fix: Added new CH5Property class
    1. Added new CH5Property class
    2. Removed unwanted console.log

--- src/ch5-common/ch5-signal-attribute-registry.ts
+++ src/ch5-common/ch5-signal-attribute-registry.ts
@@ -7,103 +7,102 @@
 
 
 interface Ch5SignalElementAttributeRegistryEntry {
-    readonly direction?: "event" | "state";
-    readonly booleanJoin?: number;
-    readonly numericJoin?: number;
-    readonly stringJoin?: number; 
-    readonly contractName?: boolean;
+	readonly direction?: "event" | "state";
+	readonly booleanJoin?: number;
+	readonly numericJoin?: number;
+	readonly stringJoin?: number;
+	readonly contractName?: boolean;
 };
 
 export interface Ch5SignalElementAttributeRegistryEntries {
-    [attributeName: string]: Ch5SignalElementAttributeRegistryEntry;
+	[attributeName: string]: Ch5SignalElementAttributeRegistryEntry;
 };
 
 export interface CH5SignalElementDefaultAttributeEntry {
-    attributes: string[]; 
-    defaultValue: string;
+	attributes: string[];
+	defaultValue: string;
 };
 
 export interface CH5SignalElementDefaultAttributeEntries {
-    contractName?: CH5SignalElementDefaultAttributeEntry;
-    booleanJoin?: CH5SignalElementDefaultAttributeEntry;
-    numericJoin?: CH5SignalElementDefaultAttributeEntry;
-    stringJoin?: CH5SignalElementDefaultAttributeEntry;
+	contractName?: CH5SignalElementDefaultAttributeEntry;
+	booleanJoin?: CH5SignalElementDefaultAttributeEntry;
+	numericJoin?: CH5SignalElementDefaultAttributeEntry;
+	stringJoin?: CH5SignalElementDefaultAttributeEntry;
 }
 
 export interface CH5SignalElementRegistryEntry {
-    attributes: Ch5SignalElementAttributeRegistryEntries; 
-    addAttributeWhen?: CH5SignalElementDefaultAttributeEntries;
+	attributes: Ch5SignalElementAttributeRegistryEntries;
+	addAttributeWhen?: CH5SignalElementDefaultAttributeEntries;
 }
 
 interface RegistryEntries {
-    [elementName: string]: CH5SignalElementRegistryEntry;
+	[elementName: string]: CH5SignalElementRegistryEntry;
 }
 
 export class Ch5SignalAttributeRegistry {
 
-    public static readonly BOOLEAN_JOIN = "booleanJoin"; 
-    public static readonly NUMERIC_JOIN = "numericJoin"; 
-    public static readonly STRING_JOIN = "stringJoin"; 
-    public static readonly CONTRACT_NAME = "contractName"; 
+	public static readonly BOOLEAN_JOIN = "booleanJoin";
+	public static readonly NUMERIC_JOIN = "numericJoin";
+	public static readonly STRING_JOIN = "stringJoin";
+	public static readonly CONTRACT_NAME = "contractName";
 
-    private static readonly CUSTOM_ATTRIBUTE_PSUEDO_ELEMENT_NAME = "*";
+	private static readonly CUSTOM_ATTRIBUTE_PSUEDO_ELEMENT_NAME = "*";
 
-    //#region Singleton
-    private static _instance: Ch5SignalAttributeRegistry;
-    public static get instance(): Ch5SignalAttributeRegistry {
-        if (Ch5SignalAttributeRegistry._instance === undefined) {
-            Ch5SignalAttributeRegistry._instance = new Ch5SignalAttributeRegistry();
-        }
-        return Ch5SignalAttributeRegistry._instance;
-    } 
+	//#region Singleton
+	private static _instance: Ch5SignalAttributeRegistry;
+	public static get instance(): Ch5SignalAttributeRegistry {
+		if (Ch5SignalAttributeRegistry._instance === undefined) {
+			Ch5SignalAttributeRegistry._instance = new Ch5SignalAttributeRegistry();
+		}
+		return Ch5SignalAttributeRegistry._instance;
+	}
 
 
-    private _registry: RegistryEntries;
-    private constructor() {
-        this._registry = {};
-    }
-    //#endregion
+	private _registry: RegistryEntries;
+	private constructor() {
+		this._registry = {};
+	}
+	//#endregion
 
-    //#region public methods
-    public addElementAttributeEntries(elementName: string, entries: Ch5SignalElementAttributeRegistryEntries) {
-        if (!this._registry[elementName.toUpperCase()]) {
-            this._registry[elementName.toUpperCase()] = {attributes: entries};
-        }
-        else {
-            this._registry[elementName.toUpperCase()].attributes = entries;
-        }
-    }
-    public addCustomAttributeEntry(attributeName: string, entry: Ch5SignalElementAttributeRegistryEntry) {
-        if (!this._registry[Ch5SignalAttributeRegistry.CUSTOM_ATTRIBUTE_PSUEDO_ELEMENT_NAME]) {
-            this._registry[Ch5SignalAttributeRegistry.CUSTOM_ATTRIBUTE_PSUEDO_ELEMENT_NAME] = { attributes: {} }; 
-        }
-        this._registry[Ch5SignalAttributeRegistry.CUSTOM_ATTRIBUTE_PSUEDO_ELEMENT_NAME].attributes[attributeName] 
-            = entry;
-    }
+	//#region public methods
+	public addElementAttributeEntries(elementName: string, entries: Ch5SignalElementAttributeRegistryEntries) {
+		if (!this._registry[elementName.toUpperCase()]) {
+			this._registry[elementName.toUpperCase()] = { attributes: entries };
+		}
+		else {
+			this._registry[elementName.toUpperCase()].attributes = entries;
+		}
+	}
+	public addCustomAttributeEntry(attributeName: string, entry: Ch5SignalElementAttributeRegistryEntry) {
+		if (!this._registry[Ch5SignalAttributeRegistry.CUSTOM_ATTRIBUTE_PSUEDO_ELEMENT_NAME]) {
+			this._registry[Ch5SignalAttributeRegistry.CUSTOM_ATTRIBUTE_PSUEDO_ELEMENT_NAME] = { attributes: {} };
+		}
+		this._registry[Ch5SignalAttributeRegistry.CUSTOM_ATTRIBUTE_PSUEDO_ELEMENT_NAME].attributes[attributeName] = entry;
+	}
 
-    public addElementDefaultAttributeEntries(elementName: string, entries: CH5SignalElementDefaultAttributeEntries) {
-        if (!this._registry[elementName.toUpperCase()]) {
-            this._registry[elementName.toUpperCase()] = {attributes: {}};
-        }
-        this._registry[elementName.toUpperCase()].addAttributeWhen = entries;
-    }
+	public addElementDefaultAttributeEntries(elementName: string, entries: CH5SignalElementDefaultAttributeEntries) {
+		if (!this._registry[elementName.toUpperCase()]) {
+			this._registry[elementName.toUpperCase()] = { attributes: {} };
+		}
+		this._registry[elementName.toUpperCase()].addAttributeWhen = entries;
+	}
 
-    public getElementAttributeEntry(elementName: string, attributeName: string): Ch5SignalElementAttributeRegistryEntry | undefined {
-        if (this._registry[elementName] !== undefined) {
-            return this._registry[elementName].attributes[attributeName];
-        }
-        else if (this._registry[Ch5SignalAttributeRegistry.CUSTOM_ATTRIBUTE_PSUEDO_ELEMENT_NAME] !== undefined) {
-            // the custom crestron attributes like data-ch5-show are registered here
-            return this._registry[Ch5SignalAttributeRegistry.CUSTOM_ATTRIBUTE_PSUEDO_ELEMENT_NAME].attributes[attributeName];
-        }
-        return undefined;
-    }
+	public getElementAttributeEntry(elementName: string, attributeName: string): Ch5SignalElementAttributeRegistryEntry | undefined {
+		if (this._registry[elementName] !== undefined) {
+			return this._registry[elementName].attributes[attributeName];
+		}
+		else if (this._registry[Ch5SignalAttributeRegistry.CUSTOM_ATTRIBUTE_PSUEDO_ELEMENT_NAME] !== undefined) {
+			// the custom crestron attributes like data-ch5-show are registered here
+			return this._registry[Ch5SignalAttributeRegistry.CUSTOM_ATTRIBUTE_PSUEDO_ELEMENT_NAME].attributes[attributeName];
+		}
+		return undefined;
+	}
 
-    public getElementDefaultAttributeEntries(elementName: string): CH5SignalElementDefaultAttributeEntries | undefined {
-        if (this._registry[elementName] !== undefined) {
-            return this._registry[elementName].addAttributeWhen;
-        }
-        return undefined;
-    }
-    //#endregion
+	public getElementDefaultAttributeEntries(elementName: string): CH5SignalElementDefaultAttributeEntries | undefined {
+		if (this._registry[elementName] !== undefined) {
+			return this._registry[elementName].addAttributeWhen;
+		}
+		return undefined;
+	}
+	//#endregion
 }
