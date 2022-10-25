35833544f5b5961138e15a374ece16ff1477d73a
Raghavendra Kumar Dabbir <rdabbir@crestron.com>
2022-10-25 12:18:08 +0530

    fix: Changes to support Code merge
    Changes to support Code merge

--- src/_interfaces/index.ts
+++ src/_interfaces/index.ts
@@ -33,7 +33,7 @@ export * from "./../ch5-modal-dialog/interfaces/i-ch5-modal-dialog-documentation
 export * from "./../ch5-overlay-panel/interfaces/i-ch5-overlay-panel-documentation";
 export * from "./../ch5-select/interfaces/i-ch5-select-documentation";
 export * from "./../ch5-select-option/interfaces/i-ch5-select-option-documentation";
-export * from "./../ch5-signal-gauge/interfaces/i-ch5-signal-gauge-documentation";
+export * from "./../ch5-signal-level-gauge/interfaces/i-ch5-signal-level-gauge-documentation";
 export * from "./../ch5-slider/interfaces/i-ch5-slider-documentation";
 export * from "./../ch5-spinner/interfaces/i-ch5-spinner-documentation";
 export * from "./../ch5-template/interfaces/i-ch5-template-documentation";
