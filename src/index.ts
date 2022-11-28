ba94bf5db90cfff79fbf3e59c16d085a6a49c76e
Shivangi Mishra <67198842+smishra-crestron@users.noreply.github.com>
2022-11-28 19:15:47 +0530

    FIX: CH5C-3454
    https://crestroneng.atlassian.net/browse/CH5C-3454

--- src/index.ts
+++ src/index.ts
@@ -40,3 +40,4 @@ export * from './ch5-jointotext-numeric';
 export * from './ch5-animation';
 // Shared models used for WCT tests
 export * from './utility-models/index'; // Use CrComLib.Ch5RoleAttributeMapping to access
+export * from './ch5-wifi-signal-level-gauge';
