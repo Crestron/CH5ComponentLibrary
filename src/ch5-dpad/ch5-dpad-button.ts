009b4e4e66e91907c26514d425e54f0d8919cc6f
Sprasaanth <prasaanth.santhosh@crestron.com>
2023-04-03 12:27:23 +0530

    minor code changes in dpad-button
    Made changes in dpad-button and dpad-child-base as requested

--- src/ch5-dpad/ch5-dpad-button.ts
+++ src/ch5-dpad/ch5-dpad-button.ts
@@ -9,9 +9,7 @@ import _ from "lodash";
 import { Ch5Common } from "../ch5-common/ch5-common";
 import { Ch5SignalAttributeRegistry } from "../ch5-common/ch5-signal-attribute-registry";
 import { Ch5DpadChildBase } from "./ch5-dpad-child-base";
-import { CH5DpadUtils } from "./ch5-dpad-utils";
 import { ICh5DpadChildBaseAttributes } from "./interfaces/i-ch5-dpad-child-base-attributes";
-import { TCh5DpadChildButtonType } from "./interfaces/t-ch5-dpad";
 import { ICh5PropertySettings } from "../ch5-core/ch5-property";
 
 export class Ch5DpadButton extends Ch5DpadChildBase implements ICh5DpadChildBaseAttributes {
@@ -28,58 +26,15 @@ export class Ch5DpadButton extends Ch5DpadChildBase implements ICh5DpadChildBase
 		center: 'fa-circle'
 	};
 
-	public static readonly COMPONENT_PROPERTIES: ICh5PropertySettings[] = [
-		{
-			default: "",
-			name: "key",
-			removeAttributeOnNull: true,
-			type: "string",
-			valueOnAttributeEmpty: "",
-			isObservableProperty: true
-		},
-		{
-			default: "",
-			name: "iconClass",
-			removeAttributeOnNull: true,
-			type: "string",
-			valueOnAttributeEmpty: "",
-			isObservableProperty: true
-		},
-		{
-			default: "",
-			name: "iconUrl",
-			removeAttributeOnNull: true,
-			type: "string",
-			valueOnAttributeEmpty: "",
-			isObservableProperty: true
-		},
-		{
-			default: "",
-			name: "label",
-			removeAttributeOnNull: true,
-			type: "string",
-			valueOnAttributeEmpty: "",
-			isObservableProperty: true
-		},
-		{
-			default: false,
-			name: "pressed",
-			removeAttributeOnNull: true,
-			type: "boolean",
-			valueOnAttributeEmpty: false,
-			isObservableProperty: true
-		},
-		{
-			default: "",
-			name: "sendEventOnClick",
-			removeAttributeOnNull: true,
-			type: "string",
-			valueOnAttributeEmpty: "",
-			isObservableProperty: true
-		}
-	];
-
 	//#endregion
+
+	public static registerCustomElement() {
+		if (typeof window === "object"
+			&& typeof window.customElements === "object"
+			&& typeof window.customElements.define === "function") {
+			window.customElements.define(Ch5DpadButton.ELEMENT_NAME, Ch5DpadButton);
+		}
+	}
 
 	//#region 2. Lifecycle Hooks
 
@@ -94,9 +49,9 @@ export class Ch5DpadButton extends Ch5DpadChildBase implements ICh5DpadChildBase
 	static get observedAttributes() {
 		const inheritedObsAttrs = Ch5Common.observedAttributes;
 		const newObsAttrs: string[] = [];
-		for (let i: number = 0; i < Ch5DpadButton.COMPONENT_PROPERTIES.length; i++) {
-			if (Ch5DpadButton.COMPONENT_PROPERTIES[i].isObservableProperty === true) {
-				newObsAttrs.push(Ch5DpadButton.COMPONENT_PROPERTIES[i].name.toLowerCase());
+		for (let i: number = 0; i < Ch5DpadChildBase.COMPONENT_PROPERTIES.length; i++) {
+			if (Ch5DpadChildBase.COMPONENT_PROPERTIES[i].isObservableProperty === true) {
+				newObsAttrs.push(Ch5DpadChildBase.COMPONENT_PROPERTIES[i].name.toLowerCase());
 			}
 		}
 		return inheritedObsAttrs.concat(newObsAttrs);
@@ -106,19 +61,10 @@ export class Ch5DpadButton extends Ch5DpadChildBase implements ICh5DpadChildBase
 		this.logger.start("attributeChangedCallback", this.primaryCssClass);
 		if (oldValue !== newValue) {
 			this.logger.log('ch5-dpad-button attributeChangedCallback("' + attr + '","' + oldValue + '","' + newValue + '")');
-			const attributeChangedProperty = Ch5DpadButton.COMPONENT_PROPERTIES.find((property: ICh5PropertySettings) => { return property.name.toLowerCase() === attr.toLowerCase() && property.isObservableProperty === true });
+			const attributeChangedProperty = Ch5DpadChildBase.COMPONENT_PROPERTIES.find((property: ICh5PropertySettings) => { return property.name.toLowerCase() === attr.toLowerCase() && property.isObservableProperty === true });
 			if (attributeChangedProperty) {
 				const thisRef: any = this;
 				const key = attributeChangedProperty.name;
-				if (key === "key") {
-					CH5DpadUtils.createIconTag(this);
-					super.initializeParams({
-						primaryTagClass: newValue as TCh5DpadChildButtonType,
-						defaultIconClass: Ch5DpadButton.DEFAULT_ICONS[newValue as TCh5DpadChildButtonType],
-						defaultArrowClass: newValue === 'center' ? '' : 'direction-btn',
-						btnType: newValue as TCh5DpadChildButtonType
-					});
-				}
 				thisRef[key] = newValue;
 			} else {
 				super.attributeChangedCallback(attr, oldValue, newValue);
@@ -131,10 +77,5 @@ export class Ch5DpadButton extends Ch5DpadChildBase implements ICh5DpadChildBase
 
 }
 
-if (typeof window === "object"
-	&& typeof window.customElements === "object"
-	&& typeof window.customElements.define === "function") {
-	window.customElements.define(Ch5DpadButton.ELEMENT_NAME, Ch5DpadButton);
-}
-
+Ch5DpadButton.registerCustomElement();
 Ch5DpadButton.registerSignalAttributeTypes();
