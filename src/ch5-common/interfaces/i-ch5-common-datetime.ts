76f3a0139c866d2a8b9837807f1462e8016a899e
rbarla-crestron <rbarla@crestron.com>
2023-08-04 15:57:08 +0530

    typo updated

--- src/ch5-common/interfaces/i-ch5-common-datetime.ts
+++ src/ch5-common/interfaces/i-ch5-common-datetime.ts
@@ -9,11 +9,11 @@ import { ICh5CommonForClass } from "./common/i-ch5-common-class";
 import { ICh5CommonForDebug } from "./common/i-ch5-common-debug";
 import { ICh5CommonForRole } from "./common/i-ch5-common-role";
 import { ICh5CommonForStyle } from "./common/i-ch5-common-style";
-import { ICh5AttributesDatetime } from "./i-ch5-common-attributes-datetime";
+import { ICh5AttributesDateTime } from "./i-ch5-common-attributes-datetime";
 
 /**
  * @ignore
  */
-export interface ICh5CommonDateTime extends ICh5CommonForDebug, ICh5CommonForRole, ICh5CommonForStyle, ICh5CommonForClass, ICh5AttributesDatetime {
+export interface ICh5CommonDateTime extends ICh5CommonForDebug, ICh5CommonForRole, ICh5CommonForStyle, ICh5CommonForClass, ICh5AttributesDateTime {
 
 }
