6cb18cea1d0aec9390ad12262b1c1a3b6bfa1933
sachin kalekar <skalekar@crestron.com>
2023-09-07 12:18:16 +0530

    CH5C-529 remove invalid attributes from ch5-templates

--- src/ch5-common/interfaces/i-ch5-common-attributes-wo-receivestateenable.ts
+++ src/ch5-common/interfaces/i-ch5-common-attributes-wo-receivestateenable.ts
@@ -6,13 +6,9 @@
 // under which you licensed this source code.
 
 import { ICh5CommonAttributesForAppendClassWhenInViewPort } from "./common/i-ch5-common-attributes-appendclasswheninviewport";
-import { ICh5CommonAttributesForCustomClass } from "./common/i-ch5-common-attributes-customclass";
-import { ICh5CommonAttributesForCustomStyle } from "./common/i-ch5-common-attributes-customstyle";
 import { ICh5CommonAttributesForDir } from "./common/i-ch5-common-attributes-dir";
 import { ICh5CommonAttributesForId } from "./common/i-ch5-common-attributes-id";
 import { ICh5CommonAttributesForNoShowType } from "./common/i-ch5-common-attributes-noshowtype";
-import { ICh5CommonAttributesForReceiveStateCustomClass } from "./common/i-ch5-common-attributes-receivestatecustomclass";
-import { ICh5CommonAttributesForReceiveStateCustomStyle } from "./common/i-ch5-common-attributes-receivestatecustomstyle";
 import { ICh5CommonAttributesForReceiveStateHidePulse } from "./common/i-ch5-common-attributes-receivestatehidepulse";
 import { ICh5CommonAttributesForReceiveStateShow } from "./common/i-ch5-common-attributes-receivestateshow";
 import { ICh5CommonAttributesForReceiveStateShowPulse } from "./common/i-ch5-common-attributes-receivestateshowpulse";
@@ -22,18 +18,12 @@ import { ICh5CommonAttributesForShow } from "./common/i-ch5-common-attributes-sh
 /**
  * @ignore
  */
-export interface ICh5CommonAttributesWoReceiveStateEnable extends ICh5CommonAttributesForDir, 
-																							ICh5CommonAttributesForShow, 
-																							ICh5CommonAttributesForAppendClassWhenInViewPort,
-																							ICh5CommonAttributesForSendEventOnShow,
-																							ICh5CommonAttributesForReceiveStateHidePulse,
-																							ICh5CommonAttributesForReceiveStateShowPulse,
-																							ICh5CommonAttributesForReceiveStateShow,
-																							ICh5CommonAttributesForReceiveStateCustomStyle,
-																							ICh5CommonAttributesForReceiveStateCustomClass,
-																							ICh5CommonAttributesForNoShowType,
-																							ICh5CommonAttributesForCustomStyle, 
-																							ICh5CommonAttributesForCustomClass, 
-																							ICh5CommonAttributesForId {
-
-}
+export interface ICh5CommonAttributesWoReceiveStateEnable extends ICh5CommonAttributesForDir,
+	ICh5CommonAttributesForShow,
+	ICh5CommonAttributesForAppendClassWhenInViewPort,
+	ICh5CommonAttributesForSendEventOnShow,
+	ICh5CommonAttributesForReceiveStateHidePulse,
+	ICh5CommonAttributesForReceiveStateShowPulse,
+	ICh5CommonAttributesForReceiveStateShow,
+	ICh5CommonAttributesForNoShowType,
+	ICh5CommonAttributesForId {}
