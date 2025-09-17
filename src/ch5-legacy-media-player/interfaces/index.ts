aea130220c068fb58c57344aeed351230c8eca13
rbarla-crestron <rbarla@crestron.com>
2025-09-17 11:05:12 +0530

    removed child elements

--- src/ch5-legacy-media-player/interfaces/index.ts
+++ src/ch5-legacy-media-player/interfaces/index.ts
@@ -1,6 +1,2 @@
 export * from './i-ch5-legacy-media-player-attributes';
 export * from './i-ch5-legacy-media-player-documentation';
-export * from './i-ch5-legacy-media-player-my-music-attributes';
-export * from './i-ch5-legacy-media-player-my-music-documentation';
-export * from './i-ch5-legacy-media-player-now-playing-attributes';
-export * from './i-ch5-legacy-media-player-now-playing-documentation';
