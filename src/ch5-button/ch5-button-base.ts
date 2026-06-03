7d2e0d789a51f735a13af7246356f04d4564b5eb
Jyothi Prakash <jthaduri@crestron.com>
2026-06-03 15:35:25 +0530

    changes for number of lines text calculation changes for multiline support

--- src/ch5-button/ch5-button-base.ts
+++ src/ch5-button/ch5-button-base.ts
@@ -3073,8 +3073,8 @@ export class Ch5ButtonBase extends Ch5Common implements ICh5ButtonAttributes {
 	public fitEllipsisForMultiline() {
 		let numberOfLines = 0;
 		const lineHeight = this.getLineHeightSuper(this._elSpanForLabelOnly);
-		//const topAndBottomPadding = 20;
-		const containerHeight = this.getContainerHeight(this._elButton);
+		const topAndBottomPadding = 20;
+		const containerHeight = this.getContainerHeight(this._elButton) - topAndBottomPadding;
 		if (containerHeight < lineHeight) {
 			numberOfLines = 1
 		} else {
